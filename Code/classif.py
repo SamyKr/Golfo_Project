e638a93bdc17bc4750b7b5f372533cfac46768d6
abigaelle-lagneaux <168813468+abigaelle-lagneaux@users.noreply.github.com>
2025-03-25 12:06:29 +0100

    algo classif A MODIFIER

--- Code/classif.py
+++ Code/classif.py
@@ -1,43 +1,97 @@
-import matplotlib.pyplot as plt
+import os
+import cv2
 import numpy as np
-from matplotlib.backend_bases import MouseEvent
+import time
+from skimage.io import imread, imsave
+from sklearn.cluster import DBSCAN
+from skimage.measure import find_contours
+from skimage.morphology import binary_erosion
+from skimage.color import label2rgb, rgb2hsv
+from skimage.segmentation import slic
 
-def enregistrer_points(image_path):
-    # Charger l'image
-    img = plt.imread(image_path)
-
-    # Liste des points cliqués
-    points = []
-
-    # Fonction pour capturer les clics de souris
-    def on_click(event: MouseEvent):
-        # Vérifier que le clic est dans les limites de l'image
-        if event.xdata is not None and event.ydata is not None:
-            # Enregistrer le point cliqué
-            points.append((event.xdata, event.ydata))
-            print(f"Point ajouté: ({event.xdata}, {event.ydata})")
-            # Afficher le point sur l'image
-            ax.plot(event.xdata, event.ydata, 'ro')  # Marqueur rouge
-            plt.draw()
-
-    # Créer la figure et les axes
-    fig, ax = plt.subplots()
-    ax.imshow(img)
-    ax.set_title('Cliquez pour enregistrer les points')
-
-    # Connecter la fonction de clic à l'événement de la souris
-    cid = fig.canvas.mpl_connect('button_press_event', on_click)
-
-    # Afficher l'image et attendre les clics
-    plt.show()
-
-    # Retourner les points enregistrés
-    return points
+def appel_process_image(image_path, outpout_folder, image_type) :
+    if image_type == 'roche' :
+        return process_image(image_path, outpout_folder, n_segments=650000, compactness=10, window_size=3)
+    elif image_type == 'sable' :
+        return process_image(image_path, outpout_folder, n_segments=15000, compactness=10, window_size=3)
+    else :
+        print("Le type de l'image a été mal renseigné")
+        return None
 
 
-if __name__ == "__main__":
-    # Exemple d'appel à la fonction avec le nom de l'image
-    image_path = 'output_flask/test_basse_def.png'  # Remplacer par le chemin de votre image
-    points = enregistrer_points(image_path)
+def detect_blue(image):
+    """Détecte uniquement les pixels de bleu et crée un masque binaire."""
+    hsv = rgb2hsv(image)
+    
+    blue_mask = (hsv[:, :, 0] >= 0.5) & (hsv[:, :, 0] <= 0.7)
+    blue_mask &= (hsv[:, :, 1] > 0.2)
+    blue_mask &= (hsv[:, :, 2] > 0.2)
+    
+    return binary_erosion(blue_mask)
 
+def detect_brown(image):
+    """Détecte uniquement les pixels de couleur marron et crée un masque binaire."""
+    hsv = rgb2hsv(image)
+    
+    brown_mask = (hsv[:, :, 0] >= 0.05) & (hsv[:, :, 0] <= 0.12)
+    brown_mask &= (hsv[:, :, 1] < 0.4)
+    brown_mask &= (hsv[:, :, 2] > 0.2) & (hsv[:, :, 2] < 0.6)
+    
+    return brown_mask
 
+def remove_closed_contours(contours, threshold=5):
+    filtered_contours = []
+    for contour in contours:
+        if np.linalg.norm(contour[0] - contour[-1]) > threshold:
+            filtered_contours.append(contour)
+    return filtered_contours
+
+def process_image(image_path, outpout_folder, n_segments=650000, compactness=10, window_size=3):
+    print("Démarrage du processus de segmentation de l'image...")
+    
+    start_time = time.time()
+    
+    image = imread(image_path)
+    print(f"Image chargée: {image_path}")
+    
+    print("Applique SLIC pour la segmentation des superpixels...")
+    segments = slic(image, n_segments=n_segments, compactness=compactness, start_label=1)
+    
+    print("Segmentation effectuée, traitement de l'image...")
+    segmented_image = label2rgb(segments, image, kind='avg')
+    
+    print("Détection des zones bleues et marron...")
+    blue_mask = detect_blue(segmented_image)
+    brown_mask = detect_brown(segmented_image)
+
+    other_mask = ~(blue_mask | brown_mask)
+
+    boundary = (blue_mask.astype(int) - brown_mask.astype(int)) + (brown_mask.astype(int) - other_mask.astype(int))
+
+    print("Recherche des contours...")
+    contours = find_contours(boundary, level=0)
+    contours = remove_closed_contours(contours)
+    
+    print(f"{len(contours)} contours trouvés.")
+
+    if contours:
+        largest_contour = max(contours, key=len)
+        print("Le plus grand contour trouvé, dessin de la polyligne...")
+
+        # Sauvegarder l'image résultante avec les contours dessinés
+        image_with_contour = image.copy()
+        for contour in largest_contour:
+            image_with_contour[int(contour[0]), int(contour[1])] = [255, 0, 0]  # Dessiner en rouge
+            
+        output_image = os.path.basename(image_path)
+
+        imsave(output_image, image_with_contour)
+    
+    if image_with_contour is None:
+        print("Aucun contour trouvé, retourner une image vide ou l'originale.")
+
+    end_time = time.time()
+    execution_time = (end_time - start_time) / 60
+    print(f"Temps d'exécution du traitement: {execution_time:.4f} minutes.")
+    
+    return image_with_contour, contours
